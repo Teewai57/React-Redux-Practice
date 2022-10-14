476132d92c40bb6b9f3a68cc5e2da15be0349034
Teewai57 <bobtoyoabasi@gmail.com>
2022-10-14 12:31:17 +0100

    added reducer and action folders

--- src/index.js
+++ src/index.js
@@ -2,40 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
-
-//STORE -> GLOBALIZED STATE, WHERE ALL DATA IS STORED
-
-//ACTION -> INCREMENT, DESCRIBING WHAT I PLAN TO DO
-const increment = () => {
-  return{
-      type: 'INCREMENT'
-  }
-}
-const decrement = () => {
-  return{
-      type: 'DECREMENT'
-  }
-}
-//REDUCER 
-const counter = (state = 0, action) => {
-  switch(action.type){
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-};
-let store = createStore(counter);
-store.subscribe(() => console.log(store.getState()));
-//DISPATCH
-
-store.dispatch(increment())
-
-
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
