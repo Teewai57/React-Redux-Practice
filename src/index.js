e94df038f552d41a5a282c2210d1e34718d46a9c
Teewai57 <bobtoyoabasi@gmail.com>
2022-10-14 12:46:34 +0100

    combined reducers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import allReducers from './Reducers';
+
+
+const store = createStore(allReducers)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
